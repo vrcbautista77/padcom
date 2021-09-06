daf6725c2efe274aa8eeaac2090b3f1c126a727e
rmontemayor-kezar <78962018+rmontemayor-kezar@users.noreply.github.com>
2021-09-06 15:40:57 +0800

    feat: app logo

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,25 +1,67 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:padcom/provider/user.dart';
 
-class HomePage extends StatelessWidget {
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: null,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-            constraints: BoxConstraints(
-              maxHeight: max(MediaQuery.of(context).size.width / 1.5 + 450, MediaQuery.of(context).size.height),
-            ),
-            child: Text("Home Page")
-          ),
-        ),
-      ),
-    );
+class HomePage extends StatefulWidget {
+ @override
+ State<StatefulWidget> createState() {
+    return _HomeState();
   }
+}
+
+class _HomeState extends State<HomePage> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    PlaceholderWidget(Colors.white),
+    PlaceholderWidget(Colors.deepOrange),
+    PlaceholderWidget(Colors.green)
+  ];
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(
+       leading: SizedBox(),
+       title: Text('BiCommApps'),
+       centerTitle: true,
+     ),
+     body: _children[_currentIndex], // new
+     bottomNavigationBar: BottomNavigationBar(
+       onTap: onTabTapped, // new
+       currentIndex: _currentIndex, // new
+       items: [
+         new BottomNavigationBarItem(
+           icon: Icon(Icons.home),
+           title: Text('Home'),
+         ),
+         new BottomNavigationBarItem(
+           icon: Icon(Icons.mail),
+           title: Text('Messages'),
+         ),
+         new BottomNavigationBarItem(
+           icon: Icon(Icons.person),
+           title: Text('Profile')
+         )
+       ],
+     ),
+   );
+ }
+}
+
+class PlaceholderWidget extends StatelessWidget {
+ final Color color;
+
+ PlaceholderWidget(this.color);
+
+ @override
+ Widget build(BuildContext context) {
+   return Container(
+     color: color,
+   );
+ }
 }
